bc39b8e9e906570d05d3cd713b81220bb43d7777
ROHIT POTTER <rohitpotter12@gmail.com>
2018-07-28 13:17:10 +0530

    starting learning python

---
+++ python/add.py
@@ -0,0 +1,6 @@
+number1 = input(" Please Enter the First Number: ")
+number2 = input(" Please Enter the second number: ")
+ 
+# Using arithmetic + Operator to add two numbers
+sum = float(number1) + float(number2)
+print('The sum of {0} and {1} is {2}'.format(number1, number2, sum))
