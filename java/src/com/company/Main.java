aaa3810bd681658c796c600b89d92262b30ae49e
rpotter12 <rohitpotter12@gmail.com>
2018-09-09 00:50:48 +0530

    arrayList challenge

---
+++ java/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// create a program that implements a simple mobile phone with the following capabilities.
+    // able to store, modify,remove and query contact names
+    // you will want to create a separate class for contacts(name and phone number)
+        //create a master class that holds the arraylist of contact
+        // the mobile phone class has the functinality listed below
+        // add a menu of option that are available
+        // options: quit, print list of contacts, add new contact, update, remove, search
+        // mobile phone should do everything with contact objects only
+    }
+}
