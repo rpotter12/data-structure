404b279d472404e9050a30f8358b41ed521b29a5
rpotter12 <rohitpotter12@gmail.com>
2019-08-06 20:59:11 +0530

    aranges files

--- cpp/basic c++ syntax/helloworld.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-	cout<<"Hello World!"; //used to print text
-	return 0;
-}
--- rust/src/tuples.rs
+++
@@ -1,9 +0,0 @@
-// tuples group together values of different types
-// max 12 elements
-
-pub fn run()
-{
-    let person: (&str, &str, i8) = ("brad", "mass", 37);
-
-    println!("{} id from {} and is {}", person.0, person.1, person.2);
-}
